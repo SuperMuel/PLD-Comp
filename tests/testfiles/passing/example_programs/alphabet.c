9db66e724d1045d50b73d371515ee234b2c28940
Artur Vieira Pereira <pereiraartur17@gmail.com>
2024-04-10 22:36:35 +0200

    Added remaining example programs

---
+++ tests/testfiles/passing/example_programs/alphabet.c
@@ -0,0 +1,15 @@
+int alphabet(int n) {
+  char a;
+  a = 'A';
+  while (a < 'A' + n) {
+    putchar(a);
+    a = a + 1;
+  }
+  return a;
+}
+
+int main() {
+  int c;
+  c = alphabet(15);
+  return c;
+}
